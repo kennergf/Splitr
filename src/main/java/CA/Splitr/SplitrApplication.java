8f2ce5c1806f0ecef3fb2161627f0dc426448447
kennergf <kenner.g.figueiredo@gmail.com>
2021-05-05 17:36:48 +0100

    Created project with sping, dependencies: Security, Web

---
+++ src/main/java/CA/Splitr/SplitrApplication.java
@@ -0,0 +1,13 @@
+package CA.Splitr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CA/Splitr/SplitrApplicationTests.java
@@ -0,0 +1,13 @@
+package CA.Splitr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
