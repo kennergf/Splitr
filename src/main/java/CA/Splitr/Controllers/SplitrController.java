a6b3c2d5075955fc919913fde817ea09cd981a4d
kennergf <kenner.g.figueiredo@gmail.com>
2021-05-07 21:44:41 +0100

    Changed gradle version, and changes on controller

--- src/main/java/CA/Splitr/Controllers/SplitrController.java
+++ src/main/java/CA/Splitr/Controllers/SplitrController.java
@@ -3,7 +3,10 @@ package CA.Splitr.Controllers;
 import java.util.ArrayList;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +15,17 @@ import CA.Splitr.Models.Expense;
 
 @RestController
 public class SplitrController {
+
+    @PostMapping("/{label}/expense")
     public Map<String, ArrayList<Expense>> addExpense(@PathVariable("label") String label,
     @RequestHeader(name = "Authorization", required = true) String token,
     @RequestBody(required = true) Expense expense){
         return null;
     }
+
+    @CrossOrigin("*")
+    @GetMapping("/{label}/summary")
+	public String getSummary(@PathVariable("label") String label) {
+		return label;
+	}
 }
